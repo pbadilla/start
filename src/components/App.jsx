55cac792ee6c44e04b3c3554bea6e761e71c9bc2
Fardin Mohammed <fardeenmohdpl@gmail.com>
2021-08-08 16:32:08 +0200

    Add React, Webpack, Babel, Sass

---
+++ src/components/App.jsx
@@ -0,0 +1,21 @@
+import "../styles/index.scss";
+import React from "react";
+// import Recipes from "./Recipes";
+// import pic from "../images/pic2.jpg";
+
+const App = () => {
+  return (
+    <>
+      {/* <section className="hero"></section> */}
+      <main>
+        <section>
+          <h1>Hello its React</h1>
+        </section>
+      </main>
+      {/* <img src={pic} /> */}
+      {/* <Recipes /> */}
+    </>
+  );
+};
+
+export default App;
